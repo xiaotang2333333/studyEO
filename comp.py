af7999434e556bc0809e8e1d63b957c013c2f715
xiaotang2333333 <657177648@qq.com>
2025-11-24 19:50:45 +0800

    11.24

--- comp.py
+++ comp.py
@@ -1,15 +1,10 @@
 import sympy as sp
 import numpy as np
 
-f1p,f2p = sp.symbols(" f_1^{\\prime} f_2^{\\prime}",real = True)
-L = 65
-lkp = 50
-d = 15
-fp = 35
-h1 = 100
-#define temp variables
-h2 = sp.symbols(" h_2",real = True)
-eq1 = sp.Eq(h1*(1 - d/f1p), h2)
-eq2 = sp.Eq(h2/lkp, h1/fp)
-eq3 = sp.Eq(1/fp,1/f1p + 1/f2p - d/(f1p*f2p))
-sol = sp.solve([eq1, eq2, eq3], (f1p, f2p, h2))
+l1,l1p,fp = sp.symbols('l_1 l_1^\\prime f^\\prime', real=True)
+eq1 = sp.Eq(l1p - l1, 900)
+eq2 = sp.Eq(1/l1p - 1/l1, 1/fp)
+l = sp.symbols('l', real=True)
+eq3 = sp.Eq(1/(l1p - l) - 1/(l1 - l), 1/fp)
+eq4 = sp.Eq((l1p)/(l1), -(l1p - l)/(l1 - l)/4)
+sol = sp.solve([eq1, eq2, eq3, eq4], (l1, l1p, fp,l))
