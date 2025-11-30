0c3982590b3d83835c6c44bc19f03fcb9b7958d2
xiaotang2333333 <657177648@qq.com>
2025-11-30 22:05:51 +0800

    校对第一章

--- comp.py
+++ comp.py
@@ -1,10 +1,7 @@
 import sympy as sp
 import numpy as np
 
-l1,l1p,fp = sp.symbols('l_1 l_1^\\prime f^\\prime', real=True)
-eq1 = sp.Eq(l1p - l1, 900)
-eq2 = sp.Eq(1/l1p - 1/l1, 1/fp)
-l = sp.symbols('l', real=True)
-eq3 = sp.Eq(1/(l1p - l) - 1/(l1 - l), 1/fp)
-eq4 = sp.Eq((l1p)/(l1), -(l1p - l)/(l1 - l)/4)
-sol = sp.solve([eq1, eq2, eq3, eq4], (l1, l1p, fp,l))
+l,lp,r = sp.symbols('l l^\\prime r', real=True)
+eq1 = sp.Eq(4,lp/l)
+eq2 = sp.Eq(1/l + 1/lp, 2/r)
+sol = sp.solve([eq1, eq2], (l, lp))
